11d6e193d1ddc8583cac1e9b0515ca6daab482b0
denmercs <denmercs@gmail.com>
2019-08-16 12:55:59 -0500

    MVP met

--- smurfs/src/components/CardInput.js
+++ smurfs/src/components/CardInput.js
@@ -1,58 +1,78 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { withFormik, Form, Field } from "formik";
+import * as Yup from "yup";
+import axios from "axios";
 
-const NewPost = () => {
-  const [state, setState] = useState({
-    name: "",
-    height: "",
-    age: null
-  });
-
-  const handleSubmit = e => {
-    e.preventDefault();
-    console.log("STATE RIGHT NOW", state);
-  };
-
-  const handleInputChange = e => {
-    setState({
-      [e.target.name]: e.target.value
+function TheForm({ errors, touched, status, setStatus }) {
+  const [smurfs, setSmurfs] = useState([]);
+  useEffect(() => {
+    axios.get("http://localhost:3333/smurfs").then(res => {
+      setStatus(res.data);
     });
-  };
-
+  }, []);
+  useEffect(() => {
+    if (status) {
+      setSmurfs([...smurfs, status]);
+    }
+  }, [status]);
   return (
-    <div>
-      <form onSubmit={handleSubmit}>
-        <div className="form-group">
-          <input
-            type="text"
-            placeholder="name"
-            name="name"
-            onChange={handleInputChange}
-          />
-        </div>
-        <div className="form-group">
-          <input
-            type="number"
-            placeholder="age"
-            name="age"
-            onChange={handleInputChange}
-          />
-        </div>
-        <div className="form-group">
-          <input
-            type="text"
-            placeholder="height"
-            name="height"
-            onChange={handleInputChange}
-          />
-        </div>
-        <div className="form-group">
-          <button type="submit" className="btn btn-primary">
-            Add Post
-          </button>
-        </div>
-      </form>
-    </div>
+    <>
+      <Form>
+        <label>
+          Smurf Name
+          {touched.name && errors.name && (
+            <p className="errors">{errors.name}</p>
+          )}
+          <Field type="text" name="name" placeholder="Smurf Name..." />
+        </label>
+        <label>
+          Smurf Age
+          {touched.age && errors.age && <p className="errors">{errors.age}</p>}
+          <Field type="text" name="age" placeholder="Smurf Age..." />
+        </label>
+        <label>
+          Smurf Height
+          {touched.height && errors.height && (
+            <p className="errors">{errors.height}</p>
+          )}
+          <Field type="text" name="height" placeholder="Smurf Height..." />
+        </label>
+        <button type="submit">Add Smurf</button>
+      </Form>
+    </>
   );
-};
+}
 
-export default NewPost;
+const FormikForm = withFormik({
+  mapPropsToValues: ({ name, age, height }) => {
+    return {
+      name: "",
+      age: "",
+      height: ""
+    };
+  },
+  validationSchema: Yup.object().shape({
+    name: Yup.string()
+      .min(3, "Please Provide A Longer Name For Smurf")
+      .required("Please Provide Smurf Name"),
+    age: Yup.string().required("Please Provide Smurf Age"),
+    height: Yup.string().required("Please Provide Smurf Height")
+  }),
+  handleSubmit(values, { resetForm, setStatus }) {
+    axios
+      .post("http://localhost:3333/smurfs", {
+        name: values.name,
+        age: values.age,
+        height: values.height
+      })
+      .then(response => {
+        console.log("response", response);
+      })
+      .catch(function(error) {
+        console.log(error);
+      });
+    resetForm();
+  }
+})(TheForm);
+
+export default FormikForm;
